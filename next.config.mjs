deff57e80c69be7f30d251603832ea7d6e95dab9
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-04-07 16:06:51 +0700

    Config Project structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/",
+                destination: "/admin",
+            },
+        ];
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        // Important: return the modified config
+        config.module.rules.push({
+            test: /\.node$/,
+            use: "file-loader",
+        });
+
+        return config;
+    },
+    images: {
+        domains: ["localhost"],
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
+    reactStrictMode: false,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    output: "standalone",
+};
 
 export default nextConfig;
